f029a2545716d816c3b4be498ac86532ad79db0e
Ed Ropple <ed@edropple.com>
2019-05-28 20:27:02 -0400

    fixed incorrect header capitalization; removed unnecessary peer dep

--- src/correlation-id.middleware.ts
+++ src/correlation-id.middleware.ts
@@ -7,7 +7,8 @@ import uuidV4 from "uuid/v4";
 export function CorrelationIdMiddleware(genFn: () => string = uuidV4) {
   return (req: any, res: any, next: () => void) => {
     const correlationHeader = req.header("x-correlation-id") || genFn();
-    req.headers["X-Correlation-Id"] = correlationHeader;
+    // make sure this is lower-cased, otherwise downstream stuff will barf.
+    req.headers["x-correlation-id"] = correlationHeader;
     res.set("X-Correlation-Id", correlationHeader);
     next();
   }
